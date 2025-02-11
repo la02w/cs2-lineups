c83209184f05ae5d841de13935a4148250e28eac
La02w <1786075785@qq.com>
2025-02-11 11:17:07 +0800

    1

--- src/maps/types.ts
+++ src/maps/types.ts
@@ -1,5 +1,23 @@
 import { mirage } from './mirage'
 
+import ancientPath from '@/assets/maps/ancient.png'
+import anubisPath from '@/assets/maps/anubis.png'
+import dust2Path from '@/assets/maps/dust2.png'
+import infernoPath from '@/assets/maps/inferno.png'
+import miragePath from '@/assets/maps/mirage.png'
+import nukePath from '@/assets/maps/nuke.png'
+import trainPath from '@/assets/maps/train.png'
+
+export const maps: { [key: string]: string } = {
+  ancient: ancientPath,
+  anubis: anubisPath,
+  dust2: dust2Path,
+  inferno: infernoPath,
+  nuke: nukePath,
+  train: trainPath,
+  mirage: miragePath,
+}
+
 export type Start = {
   marker: { x: string; y: string }
   desc: string
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import { imageToWebpPlugin } from 'vite-plugin-image-to-webp'
+import path from 'node:path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [
+    vue(),
+    tailwindcss(),
+    imageToWebpPlugin({
+      imageFormats: ['jpg', 'jpeg', 'png'],
+      webpQuality: {},
+      destinationFolder: 'dist',
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
